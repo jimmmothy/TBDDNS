6448b2836d3c13c8a1bd597cfc82e150a760f2cf
James Figures <j@figures.email>
2022-06-10 14:42:35 +1000

    tidied up readme

--- tbddns.py
+++ tbddns.py
@@ -1,34 +1,5 @@
 #!/usr/bin/python3
 
-# Helpful links along the way
-# https://pythonbasics.org/flask-rest-api/
-# https://terminalcheatsheet.com/guides/curl-rest-api
-
-'''
-Media server hosted with a dynamic IP in differant location
-Need to keep track of its IP so i can SSH/VNC in etc.
-Playing around with a basic FLASK REST-API to see how its done
-
-Endpoints
-POST /ip insert IP
-GET /ip retreive latest IP
-
-USAGE-
-Cronjob on media server running a bash script similar (obviously chance the localhost address to the 
-location the flask script is running):
-ENTRY=$(curl -s ifconfig.co) && curl -X PUT -H 'Content-Type: application/json' -d "{\"ip\" : \"${ENTRY}\"}" http://127.0.0.1:5000/ip
-
-Use something like this to obtain the IP when you need it
-curl -X GET -H 'Content-Type: application/json' http://127.0.0.1:5000/ip
-
-Things to do-
-Stop double ups
-HTTPS or authentication/security
-Return differant status codes where appropriate
-Make a class to handle file IO
-Simplify it and only use the one entry, do we really need history? probs not
-Ditch basic bitch files and use a db
-'''
 
 from ipsanity import ipsanity
 
